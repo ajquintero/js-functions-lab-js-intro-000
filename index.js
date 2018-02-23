2357343834a47cd7a96f4647bef62ee1616cb9ed
Abel Quintero <abel.j.quintero@gmail.com>
2018-02-23 00:19:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 // write your code below!
-function shout(string){
-  return string.toUpperCase()
+function happyHolidays (){
+  return "Happy holidays!"
 }
 
-function whisper(string){
-  return string.toLowerCase()
+function happyHolidaysTo (name){
+  return `Happy holidays, ${name}!`
 }
 
-function logShout(string){
-  console.log(string.toUpperCase())
+function happyHolidayTo(holiday, name){
+
 }
 
-function logWhisper(string){
-  console.log(string.toLowerCase())
-}
+function holidayCountdown(days, holiday){
 
-function sayHiToGrandma(string){
-  if (string.toLowerCase() == string){
-    return ("I can't hear you!")
-  } else if (string.toUpperCase() == string) {
-    return ("YES INDEED!")
-  } else if (string == "I love you, Grandma."){
-    return ("I love you, too.")
-  }
 }
